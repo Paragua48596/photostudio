59bca803dc4e5f0f004d8032a46edc77358982df
Samuel Gonzalez <sigpay@outlook.com>
2023-02-24 18:58:36 -0300

    Importamos el Router y lo llamamos dentro del div.App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './assets/images/logo.svg';
 import './assets/styles/App.css';
 
+//Router
+import Router from './Router';
+
 function App() {
   return (
     <div className="App">
-      
+      <Router />
     </div>
   );
 }
