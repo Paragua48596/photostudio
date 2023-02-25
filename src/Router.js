d8cbc24598a84a09d814263751330a4309ce8c92
Samuel Gonzalez <sigpay@outlook.com>
2023-02-25 14:11:29 -0300

    Importamos el Menu y el Footer. Llamamos el Menu y el Footer dentro del BrowserRouter elemento, arriba y abajo del Switch elemento respectivamente. Definimos ruta raiz / en donde cargamos el componente de Home.js, previamente importado.

--- src/Router.js
+++ src/Router.js
@@ -4,6 +4,10 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 //Componentes
 import Menu from './components/Menu';
+import Footer from './components/Footer';
+
+//Paginas
+import Home from './pages/Home';
 
 class Router extends Component {
     render() {
@@ -12,12 +16,12 @@ class Router extends Component {
 
                 {/* Menu de navegacion */}
                 <Menu />
-
+                
                 <Switch>
-                    <Route exact path='/' render={() => (
-                        <h1>Test</h1>
-                    )} />
+                    <Route exact path='/' component={Home}/>
                 </Switch>
+
+                <Footer />
             </BrowserRouter>
         )
     }
