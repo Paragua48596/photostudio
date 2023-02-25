0f4898ec10a0a725be3c6bb27477d19671b5b74a
Samuel Gonzalez <sigpay@outlook.com>
2023-02-25 13:59:42 -0300

    Creamos componente Footer y sus estilos CSS.

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                <div className="flex">
+                    <p>&copy; Samuel Gonz&aacute;lez 2023</p>
+                </div>
+            </footer>
+        )
+    }
+}
+
+export default Footer
